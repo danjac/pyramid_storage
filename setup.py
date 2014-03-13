bce1448f96a5b6005b2a23a51418d2efcf79f6b5
Dan Jacob <danjac354@gmail.com>
2014-03-13 12:00:14 +0200

    Bump to 0.0.6

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ tests_require = [
 setup(
     name='pyramid_storage',
     cmdclass={'test': PyTest},
-    version='0.0.5',
+    version='0.0.6',
     license='BSD',
     author='Dan Jacob',
     author_email='danjac354@gmail.com',
