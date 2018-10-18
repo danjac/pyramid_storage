3c92f9279ed357d81680d015f63e1909ffbacaac
Fernando Fardo <GERU\ffardo@ger-nt-dev-185.geru.local>
2018-10-18 11:04:15 -0300

    Added internal version 1.0

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ tests_require = [
 setup(
     name='pyramid_storage',
     cmdclass={'test': PyTest},
-    version='0.2.0.dev0',
+    version='1.0.0',
     license='BSD',
     author='Dan Jacob',
     author_email='danjac354@gmail.com',
