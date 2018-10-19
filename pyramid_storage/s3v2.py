c2efb14004dc925e269c5264396b4f9d942a93cc
Fernando Fardo <GERU\ffardo@ger-nt-dev-185.geru.local>
2018-10-19 12:26:39 -0300

    Moved boto3 support to self module

--- pyramid_storage/s3v2.py
+++ pyramid_storage/s3v2.py
@@ -15,10 +15,12 @@ from .extensions import resolve_extensions
 from .interfaces import IFileStorage
 from .registry import register_file_storage_impl
 
+from pyramid_storage.s3 import S3FileStorage
+
 
 def includeme(config):
 
-    impl = S3FileStorage.from_settings(
+    impl = S3V2FileStorage.from_settings(
         config.registry.settings, prefix='storage.'
     )
 
@@ -26,7 +28,7 @@ def includeme(config):
 
 
 @implementer(IFileStorage)
-class S3FileStorage(object):
+class S3V2FileStorage(S3FileStorage):
 
     @classmethod
     def from_settings(cls, settings, prefix):
@@ -45,7 +47,6 @@ class S3FileStorage(object):
             ('aws.region', False, None),
             ('aws.num_retries', False, 1),
             ('aws.timeout', False, 5),
-            ('aws.boto3', False, None),
             ('aws.signature_version', False, None),
         )
         kwargs = utils.read_settings(settings, options, prefix)
@@ -62,56 +63,12 @@ class S3FileStorage(object):
         self.extensions = resolve_extensions(extensions)
         self.conn_options = conn_options
 
-    def get_connection(self):
-        try:
-            import boto
-        except ImportError:
-            raise RuntimeError("You must have boto installed to use s3")
-
-        from boto.s3.connection import OrdinaryCallingFormat
-        from boto.s3 import connect_to_region
-
-        options = self.conn_options.copy()
-        options['is_secure'] = asbool(options['is_secure'])
-
-        if options['port']:
-            options['port'] = int(options['port'])
-        else:
-            del options['port']
-
-        if not options['host']:
-            del options['host']
-
-        if not options['boto3']:
-            del options['boto3']
-
-        if not options['signature_version']:
-            del options['signature_version']
-
-        if asbool(options.pop('use_path_style')):
-            options['calling_format'] = OrdinaryCallingFormat()
-
-        num_retries = int(options.pop('num_retries'))
-        timeout = float(options.pop('timeout'))
-
-        region = options.pop('region')
-        if region:
-            del options['host']
-            del options['port']
-            conn = connect_to_region(region, **options)
-        else:
-            conn = boto.connect_s3(**options)
-
-        conn.num_retries = num_retries
-        conn.http_connection_kwargs['timeout'] = timeout
-        return conn
-
     def get_resource(self):
 
         try:
             import boto3
         except ImportError:
-            raise RuntimeError("You must have boto3 installed to use s3 with boto3 support")
+            raise RuntimeError("You must have boto3 installed to use s3v2")
         from botocore.client import Config
 
         options = self.conn_options.copy()
@@ -126,10 +83,7 @@ class S3FileStorage(object):
         return resource
 
     def get_bucket(self):
-        if self.conn_options.get('boto3'):
-            return self.get_resource().Bucket(self.bucket_name)
-
-        return self.get_connection().get_bucket(self.bucket_name)
+        return self.get_resource().Bucket(self.bucket_name)
 
     def url(self, filename):
         """Returns entire URL of the filename, joined to the base_url
@@ -143,11 +97,12 @@ class S3FileStorage(object):
         """
 
         bucket = self.get_bucket()
-        key = bucket.get_key(filename) or bucket.new_key(filename)
+        object = bucket.Object(filename)
+        stream = object.get()['body']
 
         f = tempfile.NamedTemporaryFile(delete=False)
+        f.write(stream.read())
         f.close()
-        key.get_contents_to_filename(f.name)
 
         return open(f.name, *args)
 
@@ -162,73 +117,6 @@ class S3FileStorage(object):
         :param filename: base name of file
         """
         self.get_bucket().delete_key(filename)
-
-    def filename_allowed(self, filename, extensions=None):
-        """Checks if a filename has an allowed extension
-
-        :param filename: base name of file
-        :param extensions: iterable of extensions (or self.extensions)
-        """
-        _, ext = os.path.splitext(filename)
-        return self.extension_allowed(ext, extensions)
-
-    def file_allowed(self, fs, extensions=None):
-        """Checks if a file can be saved, based on extensions
-
-        :param fs: **cgi.FieldStorage** object or similar
-        :param extensions: iterable of extensions (or self.extensions)
-        """
-        return self.filename_allowed(fs.filename, extensions)
-
-    def extension_allowed(self, ext, extensions=None):
-        """Checks if an extension is permitted. Both e.g. ".jpg" and
-        "jpg" can be passed in. Extension lookup is case-insensitive.
-
-        :param extensions: iterable of extensions (or self.extensions)
-        """
-
-        extensions = extensions or self.extensions
-        if not extensions:
-            return True
-        if ext.startswith('.'):
-            ext = ext[1:]
-        return ext.lower() in extensions
-
-    def save(self, fs, *args, **kwargs):
-        """Saves contents of a **cgi.FieldStorage** object to the file system.
-        Returns modified filename(including folder).
-
-        Returns the resolved filename, i.e. the folder + (modified/randomized)
-        filename.
-
-        :param fs: **cgi.FieldStorage** object (or similar)
-        :param folder: relative path of sub-folder
-        :param randomize: randomize the filename
-        :param extensions: iterable of allowed extensions, if not default
-        :param acl: ACL policy (if None then uses default)
-        :param replace: replace existing key
-        :param headers: dict of s3 request headers
-        :returns: modified filename
-        """
-        return self.save_file(fs.file, fs.filename, *args, **kwargs)
-
-    def save_filename(self, filename, *args, **kwargs):
-        """Saves a filename in local filesystem to the uploads location.
-
-        Returns the resolved filename, i.e. the folder +
-        the (randomized/incremented) base name.
-
-        :param filename: local filename
-        :param folder: relative path of sub-folder
-        :param randomize: randomize the filename
-        :param extensions: iterable of allowed extensions, if not default
-        :param acl: ACL policy (if None then uses default)
-        :param replace: replace existing key
-        :param headers: dict of s3 request headers
-        :returns: modified filename
-        """
-
-        return self.save_file(open(filename, "rb"), filename, *args, **kwargs)
 
     def save_file(self, file, filename, folder=None, randomize=False,
                   extensions=None, acl=None, replace=False, headers=None, partition_sub_dir=False):
@@ -306,11 +194,11 @@ class S3FileStorage(object):
 
     def get_files_list(self, folder):
         """
-        Get a list of files from current folder
+        Get a list of files from a given folder
         :param folder:
         :return:
         """
         bucket = self.get_bucket()
-        files_list = [key.name for key in bucket.list(prefix='{}\\'.format(folder), delimiter='\\')]
+        files_list = [file.key for file in bucket.objects.all() if folder in file.key]
 
         return files_list
