e1286d21a591827bc6737ee3725c2b3261988d25
Mathieu Leplatre <mathieu@mozilla.com>
2020-06-04 11:44:49 +0200

    Back to development: 0.4.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ tests_require = [
 setup(
     name='pyramid_storage',
     cmdclass={'test': PyTest},
-    version='0.3.1',
+    version='0.4.0.dev0',
     license='BSD',
     author='Dan Jacob',
     author_email='danjac354@gmail.com',
