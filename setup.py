e77212c21d5d6eaa808bcec8d579321e939cfaa7
Mathieu Leplatre <mathieu@mozilla.com>
2023-03-22 17:33:05 +0100

    Preparing release 1.3.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ tests_require = [
 setup(
     name='pyramid_storage',
     cmdclass={'test': PyTest},
-    version='1.2.1.dev0',
+    version='1.3.0',
     license='BSD',
     author='Dan Jacob',
     author_email='danjac354@gmail.com',
